d01b78247792aae54ff998eef42d8650dfb68a25
marcianoo21 <mgrzelak218@gmail.com>
2025-04-14 01:35:10 +0200

    now should be good insertion

--- scripts/insert_vcfs.py
+++ scripts/insert_vcfs.py
@@ -2,35 +2,38 @@ import sqlite3
 import pandas as pd
 from io import StringIO
 
-# Utility: parse INFO field for relevant keys
-def parse_info(info, keys):
-    parts = dict(kv.split('=') for kv in info.split(';') if '=' in kv)
-    return [parts.get(k) for k in keys]
 
+CLINGEN_VCF_PATH = "data/VCF_clingen.vcf"
+DELFOS_VCF_PATH = "data/VCF_ulises.vcf"
+DB_PATH = "db/variants.db"
 
-conn = sqlite3.connect("db/variants.db")
+# --- Connect & Recreate Tables ---
+conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
 
-# 3. Load and insert ClinGen data
-with open("data/VCF_clingen.vcf", "r") as f:
+def parse_info(info, keys):
+    parsed = dict(kv.split("=", 1) for kv in info.split(";") if "=" in kv)
+    return [parsed.get(k) for k in keys]
+
+# --- Load and Insert ClinGen ---
+with open(CLINGEN_VCF_PATH, "r") as f:
     lines = [l for l in f if not l.startswith("##")]
 clingen_df = pd.read_csv(StringIO("".join(lines)), sep="\t")
-clingen_df[['interpretation', 'criteria_met', 'criteria_not_met', 'expert_panel']] = clingen_df['INFO'].apply(
-    lambda info: pd.Series(parse_info(info, ['INTERPRETATION', 'MET_CRITERIA', 'NOT_MET_CRITERIA', 'EXPERT_PANEL']))
-)
-clingen_df[['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'interpretation', 'criteria_met', 'criteria_not_met', 'expert_panel']]\
-    .rename(columns={'#CHROM': 'chrom', 'POS': 'pos', 'ID': 'id', 'REF': 'ref', 'ALT': 'alt'})\
+clingen_df[['interpretation', 'criteria_met', 'criteria_not_met', 'expert_panel']] = \
+    clingen_df['INFO'].apply(lambda i: pd.Series(parse_info(i, ['INTERPRETATION', 'MET_CRITERIA', 'NOT_MET_CRITERIA', 'EXPERT_PANEL'])))
+clingen_df = clingen_df.rename(columns={'#CHROM': 'chrom', 'POS': 'pos', 'ID': 'id', 'REF': 'ref', 'ALT': 'alt'})
+clingen_df[['chrom', 'pos', 'id', 'ref', 'alt', 'interpretation', 'criteria_met', 'criteria_not_met', 'expert_panel']]\
     .to_sql("ClinGen_Variants", conn, if_exists="append", index=False)
 
-# 4. Load and insert DELFOS data
-with open("data/VCF_ulises.vcf", "r") as f:
+# --- Load and Insert DELFOS ---
+with open(DELFOS_VCF_PATH, "r") as f:
     lines = [l for l in f if not l.startswith("##")]
 delfos_df = pd.read_csv(StringIO("".join(lines)), sep="\t")
-delfos_df[['phenotype', 'interpretation', 'interpretation_reason', 'clinical_actionability']] = \
-    delfos_df['INFO'].apply(lambda info: pd.Series(parse_info(info, ['PHENOTYPE', 'INTERPRETATION', 'INTERPRETATION_REASON', 'CLINICAL_ACTIONABILITY'])))
-delfos_df[['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'phenotype', 'interpretation', 'interpretation_reason', 'clinical_actionability']]\
-    .rename(columns={'#CHROM': 'chrom', 'POS': 'pos', 'ID': 'id', 'REF': 'ref', 'ALT': 'alt'})\
+delfos_df[['phenotype', 'interpretation', 'interpretation_reason', 'clinical_actionability', 'gene']] = \
+    delfos_df['INFO'].apply(lambda i: pd.Series(parse_info(i, ['PHENOTYPE', 'INTERPRETATION', 'INTERPRETATION_REASON', 'CLINICAL_ACTIONABILITY', 'GENE'])))
+delfos_df = delfos_df.rename(columns={'#CHROM': 'chrom', 'POS': 'pos', 'ID': 'id', 'REF': 'ref', 'ALT': 'alt'})
+delfos_df[['chrom', 'pos', 'id', 'ref', 'alt', 'phenotype', 'interpretation', 'interpretation_reason', 'clinical_actionability', 'gene']]\
     .to_sql("DELFOS_Variants", conn, if_exists="append", index=False)
-
-
-print("DONE!")
+    
+    
+print("Done!")
